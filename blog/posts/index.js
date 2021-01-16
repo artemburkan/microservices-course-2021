81b248b1d77586dac752e78baf06d9c763505712
Artyom Burkan <artyom.burkan@gmail.com>
2021-01-16 18:37:26 +0300

    Added microservices

---
+++ blog/posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (request, response) => {
+  response.send(posts);
+});
+
+app.post('/posts', (request, response) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = request.body;
+
+  posts[id] = {
+    id,
+    title
+  };
+
+  response.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
