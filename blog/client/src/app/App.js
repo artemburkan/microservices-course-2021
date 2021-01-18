76a41a2070d4fc06c6220abd4077b2b4e577ab66
Artyom Burkan <artyom.burkan@gmail.com>
2021-01-18 12:45:13 +0300

    Added post request

---
+++ blog/client/src/app/App.js
@@ -0,0 +1,10 @@
+import { Post } from "./components/post/Post"
+
+export const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <Post />
+    </div>
+  )
+}
---
+++ blog/comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+
+const app = express()
+app.use(bodyParser.json())
+
+const commentsByPostId = {}
+
+app.get('/posts/:id/comments', (request, response) => {
+  console.log(request.params)
+  response.send(commentsByPostId[request.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (request, response) => {
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = request.body
+
+  const comments = commentsByPostId[request.params.id] || []
+
+  comments.push({ id: commentId, content })
+
+  commentsByPostId[request.params.id] = comments
+
+  response.status(201).send(comments)
+})
+
+app.listen(4001, () => {
+  console.log('Listening on 4001')
+})
